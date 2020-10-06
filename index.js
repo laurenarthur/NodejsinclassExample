ea5181663fe66f825f57fd8c4d908e8547e4a168
laurenarthur <arthur_lauren@columbusstate.edu>
2020-10-06 15:52:09 +0000

    added my database but the task is not coming up

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //Connection Information for Mongo
 const Todo = require('./models/todo.model');
-const mongoDB =  'mongodb+srv://testConnection:b8RwqJYgo4hD1xhe@nodetodoexample-iqnde.mongodb.net/test?retryWrites=true&w=majority'
+const mongoDB =  'mongodb+srv://arthur_lauren:<password>@cluster0.3bj1d.mongodb.net/<dbname>?retryWrites=true&w=majority'
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
